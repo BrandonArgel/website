1f43114c16c57ba1ceca99ddd8d2bc179b49792f
Fernando Fernandez <frfernandezdev@gmail.com>
2021-11-09 21:06:07 -0400

    feat: added page components

---
+++ pages/components.tsx
@@ -0,0 +1,18 @@
+import { Button } from "../components/button";
+
+export function getStaticProps() {
+  return {
+    props: {
+      notFound: process.env.NODE_ENV === "production",
+    },
+  };
+}
+
+export default function Components(): JSX.Element {
+  return (
+    <div>
+      <pre>props: {}</pre>
+      <Button />
+    </div>
+  );
+}
